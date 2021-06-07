7f5fea75221cdb0655c6caccbbe669aeef358e74
emili <emili@DESKTOP-IFFD8VA>
2021-06-07 11:37:25 +0200

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int numero2 = 6;
+            double numero3 = 2.57;
+            double suma;
+            suma = num1 + numero3;
+
+            Console.WriteLine(suma);
+            String valor1 = Console.ReadLine();
+            Console.WriteLine("El numero introducido es: " + valor1);
+            int valorint = Convert.ToInt32(valor1);
+
+            Console.ReadKey();
+
+        }
+    }
+}
